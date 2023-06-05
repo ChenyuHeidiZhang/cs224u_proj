50fa226fc7c99f900665703fb64dcda5fb7d8d1b
Violet Yao <violetyao@berkeley.edu>
2023-06-05 05:52:27 +0000

    more tests

--- tests.py
+++ tests.py
@@ -3,7 +3,7 @@ import torch
 from torch.nn import CrossEntropyLoss
 from transformers import AutoTokenizer, BertConfig, BertModel, BertForPreTraining, BertForMaskedLM
 from datasets import load_dataset
-from constants import BATCH_SIZE, NUM_LAYERS
+from constants import BATCH_SIZE, NUM_LAYERS, HIDDEN_DIM
 
 
 def test_compute_clusters():
@@ -43,7 +43,7 @@ def test_layer_by_layer_equal():
     # set [PAD] tokens to be -100
     labels[labels == tokenizer.pad_token_id] = -100
     # randomly mask 15% of the input tokens to be [MASK]
-    masked_indices = torch.bernoulli(torch.full(input_ids.size(), 0.01)).bool().to(device)
+    masked_indices = torch.bernoulli(torch.full(input_ids.size(), 0.15)).bool().to(device)
     # only mask those tokens that are not [PAD], [CLS], or [SEP]
     masked_indices = masked_indices & (input_ids != tokenizer.pad_token_id) & (input_ids != tokenizer.cls_token_id) & (input_ids != tokenizer.sep_token_id)
     input_ids[masked_indices] = tokenizer.mask_token_id
@@ -51,7 +51,7 @@ def test_layer_by_layer_equal():
     assert not torch.equal(input_ids, labels)
     # set labels to -100 (ignore index) except the masked indices
     labels[~masked_indices] = -100
-        
+
     with torch.no_grad():
         extended_attention_mask = model.get_extended_attention_mask(attention_mask, input_ids.size()).to(device)
         hidden_states= model.bert.embeddings(input_ids=input_ids)
@@ -84,7 +84,63 @@ def test_layer_by_layer_equal():
 
     # print masked_lm_loss
     print("masked_lm_loss: ", masked_lm_loss)
+    print("layer_by_layer_masked_lm_loss: ", layer_by_layer_masked_lm_loss)
+
+
+def test_turn_off_neurons():
+    # load model 
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    print(device)
+    tokenizer = AutoTokenizer.from_pretrained("bert-base-uncased")
+    tokenizer.save_pretrained('tokenizer_info')
+    config = BertConfig.from_pretrained("bert-base-uncased", output_hidden_states=True)
+    model = BertForMaskedLM.from_pretrained("bert-base-uncased", config=config).to(device)
+    print("Model loaded")
+
+    # define loss function
+    loss_fct = CrossEntropyLoss() # MLM
+
+    # load data
+    yelp = load_dataset("yelp_review_full")
+    dataset = yelp["test"]["text"][:10000] # for development purpose, only use the first 10000 examples in yelp["test"]["text"]
+    print("Dataset loaded")
+
+    batch = dataset[:BATCH_SIZE]
+    temp = tokenizer.batch_encode_plus(
+                batch, add_special_tokens=True, padding=True, truncation=True, max_length=config.max_position_embeddings, return_tensors='pt', return_attention_mask=True)
+    input_ids, attention_mask = temp["input_ids"].to(device), temp["attention_mask"].to(device)
+    labels = input_ids.clone()
+    # set [PAD] tokens to be -100
+    labels[labels == tokenizer.pad_token_id] = -100
+    # randomly mask 15% of the input tokens to be [MASK]
+    masked_indices = torch.bernoulli(torch.full(input_ids.size(), 0.15)).bool().to(device)
+    # only mask those tokens that are not [PAD], [CLS], or [SEP]
+    masked_indices = masked_indices & (input_ids != tokenizer.pad_token_id) & (input_ids != tokenizer.cls_token_id) & (input_ids != tokenizer.sep_token_id)
+    input_ids[masked_indices] = tokenizer.mask_token_id
+    # assert input_ids is different from labels
+    assert not torch.equal(input_ids, labels)
+    # set labels to -100 (ignore index) except the masked indices
+    labels[~masked_indices] = -100
+
+    # mlm loss should increase as we turn off more neurons
+    for num_neurons_to_turn_off in [10, 20, 50, 100, 200, 400]:
+        with torch.no_grad():
+            extended_attention_mask = model.get_extended_attention_mask(attention_mask, input_ids.size()).to(device)
+            hidden_states= model.bert.embeddings(input_ids=input_ids)
+            neuron_indices_to_turn_off = torch.randperm(HIDDEN_DIM)[:num_neurons_to_turn_off].tolist()
+            hidden_states[:, :, neuron_indices_to_turn_off] = 0 # turn off 100 neurons in each layer
+            for layer_id in range(NUM_LAYERS - 1):
+                hidden_states = model.bert.encoder.layer[layer_id](hidden_states, attention_mask=extended_attention_mask)[0]
+                neuron_indices_to_turn_off = torch.randperm(HIDDEN_DIM)[:num_neurons_to_turn_off].tolist()
+                hidden_states[:, :, neuron_indices_to_turn_off] = 0 # turn off 100 neurons in each layer
+            hidden_states = hidden_states
+            sequence_output = hidden_states # shape: (batch_size, seq_len, hidden_size)
+            prediction_scores = model.cls(sequence_output) # shape: (batch_size, sequence_length, vocab_size)
+            # compute MLM loss
+            masked_lm_loss_turned_off = loss_fct(prediction_scores.view(-1, config.vocab_size), labels.view(-1))
+            print(f"num_neurons_to_turn_off: {num_neurons_to_turn_off} masked_lm_loss_turned_off: {masked_lm_loss_turned_off}" )
 
 if __name__ == '__main__':
     # test_compute_clusters()
     test_layer_by_layer_equal()
+    test_turn_off_neurons()
